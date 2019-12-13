99877febf7e480a1437ab920c96f8e47a88d0152
Carlos Mo <cmo@connected.io>
2019-12-13 11:59:35 -0500

    ui: Initial work to set up a custom window title bar.

---
+++ ui/src/UI/Components/Window/WindowTitleBar.xaml.cs
@@ -0,0 +1,62 @@
+﻿// <copyright file="WindowTitleBar.xaml.cs" company="Mozilla">
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, you can obtain one at http://mozilla.org/MPL/2.0/.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirefoxPrivateNetwork.UI.Components
+{
+    /// <summary>
+    /// Interaction logic for WindowTitleBar.xaml.
+    /// </summary>
+    public partial class WindowTitleBar : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WindowTitleBar"/> class.
+        /// </summary>
+        public WindowTitleBar()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDragMoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).DragMove();
+        }
+
+        private void OnMinimizeWindow(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).WindowState = WindowState.Minimized;
+        }
+
+        private void OnMaximizeWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (Window.GetWindow(this).WindowState == WindowState.Maximized)
+            {
+                Window.GetWindow(this).WindowState = WindowState.Normal;
+            }
+            else if (Window.GetWindow(this).WindowState == WindowState.Normal)
+            {
+                Window.GetWindow(this).WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void OnCloseWindow(object sender, MouseButtonEventArgs e)
+        {
+            Window.GetWindow(this).Close();
+        }
+    }
+}
--- ui/src/UI/MainWindow.xaml.cs
+++ ui/src/UI/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
 
 namespace FirefoxPrivateNetwork.UI
